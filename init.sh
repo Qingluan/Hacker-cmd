9d212fe8498640093cee62e42a06cf53ac75c530
dark.H <darkhackdevil@gmail.com>
2016-11-16 16:03:01 +0800

    add some

---
+++ init.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+echo " ------ init ------"
+Hacker --init & Hacker -uh
+
+echo "------- install expand-plugin ----"
+pip3 install fabric3 
--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 
 setup(name='hackcmds',
-    version='1.1',
+    version='2.1',
     description='a simple way to use mongo db, let db like dict',
     url='https://github.com/Qingluan/.git',
     author='Qing luan',
@@ -12,11 +12,14 @@ setup(name='hackcmds',
     license='MIT',
     zip_safe=False,
     packages=find_packages(),
-    install_requires=['Mroylib'],
+    install_requires=['mroylib-min'],
     include_package_data=True,
     entry_points={
         'console_scripts': ['Hacker=Hacker.libs.cmd:main']
     },
+		scripts = [
+        'init.sh'
+    ]
 
 )
 
